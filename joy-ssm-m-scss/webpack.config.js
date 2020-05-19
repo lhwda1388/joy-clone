3ea137285b971d22b9933b9476126d6e692c72d7
test1 <test@gmail.com>
2020-05-19 15:02:11 +0900

    setting scss

---
+++ joy-ssm-m-scss/webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', 'css', 'scss', 'sass'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true,
+      },
+      hash: true,
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    // new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss|sass)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(css|scss|sass)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: ['url-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 9999,
+  },
+}
